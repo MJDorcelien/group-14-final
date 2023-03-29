31c1ecb54b2033e885e04c3eb193388892199ce7
magser2000 <79885095+magser2000@users.noreply.github.com>
2023-03-29 18:28:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
+
+# Maggie was at the meeting on Wednesday Night
 @app.get('/')
 def index():
     return render_template('index.html')
