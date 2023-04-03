8550053a18015f508e8e8314a5c870bf4c60cff9
MJDorcelien <lydia.dorcelien@gmail.com>
2023-04-03 19:03:16 -0400

    set up the nav bar

--- app.py
+++ app.py
@@ -8,3 +8,15 @@ app = Flask(__name__)
 @app.get('/')
 def index():
     return render_template('index.html')
+
+@app.get('/courses')
+def view_all_courses():
+    return render_template('get_all_courses.html')
+
+@app.get('/join')
+def search_all_universities():
+    return render_template('join_university.html')
+
+@app.get('/friends')
+def view_all_friends():
+    return render_template('get_all_friends.html')
