cd9cda45938205446b787704290cfa78cfcc5373
MJDorcelien <lydia.dorcelien@gmail.com>
2023-05-06 21:37:08 -0400

    setting up conftest

---
+++ tests/test_get_all_courses.py
@@ -0,0 +1,6 @@
+def test_get_all_courses_empty(test_client):
+    resp=test_client.get('/courses')
+    resp_data=resp.data.decode('utf-8')
+
+    assert '<div class="row align-items-start">' in resp_data
+    assert '<div class="col">' not in resp_data
