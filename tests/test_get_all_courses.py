8aaa6eb343c7c8637a6bb1a26e74c1191dc3ecd9
magser2000 <mgroma2000@gmail.com>
2023-05-11 13:29:53 -0400

    fixing it to say not in resp_data

--- tests/test_get_all_courses.py
+++ tests/test_get_all_courses.py
@@ -3,5 +3,5 @@ def test_get_all_courses_empty(test_client):
     resp=test_client.get('/courses')
     resp_data=resp.data.decode('utf-8')
 
-    assert '<div class="row align-items-start">' in resp_data
+    assert '<div class="row align-items-start">' not in resp_data
     assert '<div class="col">' not in resp_data
