6d01cfd608957e362b0b337b4e355cbdf3481618
MJDorcelien <lydia.dorcelien@gmail.com>
2023-05-11 23:13:00 -0400

    changing declaring database for rendering

--- app.py
+++ app.py
@@ -16,13 +16,7 @@ from src.models import db, Section
 app = Flask(__name__)
 socketio=SocketIO(app)
 
-db_user=os.getenv('DB_USER')
-db_pass=os.getenv('DB_PASS')
-db_host=os.getenv('DB_HOST')
-db_port=os.getenv('DB_PORT')
-db_name=os.getenv('DB_NAME')
-app.config['SQLALCHEMY_DATABASE_URI']\
-    =f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
+app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('DATABASE_URL')
 
 app.secret_key = os.getenv('APP_SECRET')
 
